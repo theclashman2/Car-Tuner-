2497ab896a98d32404c730a629841a3c0a2e2dcc
jinsoojyi@gmail.com <jinsoojyi@gmail.com>
2015-12-17 16:11:33 -0800

    fixed repository

---
+++ js/navbardrop.js
@@ -0,0 +1,5 @@
+    $('.navbar-default .dropdown').hover(function() {
+      $(this).find('.dropdown-menu').first().stop(true, true).slideDown(150);
+    }, function() {
+      $(this).find('.dropdown-menu').first().stop(true, true).slideUp(105)
+    });
