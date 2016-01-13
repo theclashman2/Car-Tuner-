cfde86c68394d60c606cf3ff7980e770688bd420
jinsoojyi@gmail.com <jinsoojyi@gmail.com>
2016-01-13 14:59:16 -0800

    Updated javascript file for modal

--- modal.js
+++ modal.js
@@ -1,49 +1,51 @@
 function firstModal () {
 
-  var italia = document.createElement('div');
+  var italiaContainer = document.getElementById('testing');
+  italiaContainer.setAttribute('class', 'container');
+  italiaContainer.setAttribute('class', 'votesection');
+
+  var italiaRow = document.createElement('div');
+  italiaRow.setAttribute('class', 'row');
+  italiaRow.setAttribute('class', 'votesquares');
+
+  var italiaCol = document.createElement('div');
+  italiaCol.setAttribute('class', 'col-sm-6');
+
+  var italia = document.createElement('a');
   italia.setAttribute('id', 'firstModal');
   italia.setAttribute('role', 'button');
   italia.setAttribute('href', '#myModal');
   italia.setAttribute('data-toggle', 'modal');
   italia.setAttribute('data-target', '.458modal');
 
-
   var firstBox = document.createElement('div');
   firstBox.setAttribute('class', 'squareOne');
 
-
   var firstVote = document.createElement('div');
   firstVote.setAttribute('class', 'vote1');
-
 
   var italiaImage = document.createElement('img');
   italiaImage.setAttribute('src', './images/libertywalk458.jpg');
   italiaImage.setAttribute('class', 'img-responsive');
   italiaImage.setAttribute('class', 'italia458');
 
-
   var italiaCaption = document.createElement('p');
   italiaCaption.setAttribute('class', 'italiastyle');
-
 
   var italiaOuter = document.createElement('div');
   italiaOuter.setAttribute('class', 'modal');
   italiaOuter.setAttribute('tabindex', '-1');
   italiaOuter.setAttribute('role', 'dialog');
 
-
   var italiaBones = document.createElement('div');
   italiaBones.setAttribute('class', 'modal-dialog');
   italiaBones.setAttribute('class', 'modal-sm');
 
-
   var italiaContent = document.createElement('div');
   italiaContent.setAttribute('class', 'modal-content');
 
-
   var italiaHeader = document.createElement('div');
   italiaHeader.setAttribute('class', 'modal-header');
-
 
   var italiaClick = document.createElement('button');
   italiaClick.setAttribute('type', 'button');
@@ -52,34 +54,93 @@ function firstModal () {
   italiaClick.setAttribute('aria-label', 'Close');
   italiaClick.setAttribute('class', 'close');
 
-
   var italiaHide = document.createElement('span');
-  var italiaEx = createTextNode('&times;');
-
+  var italiaEx = document.createTextNode('&times;');
 
   var italiaTitle = document.createElement('h4');
   italiaTitle.setAttribute('class', 'modal-title');
-  italiaTitle.createTextNode('Modal Title');
-
+  var italiaHead = document.createTextNode('Modal Title');
 
   var italiaBody = document.createElement('div');
   italiaBody.setAttribute('class', 'modal-body');
-  italiaBody.createElement('h4');
-  italiaBody.createTextNode('Thank you for voting!');
-
+  var italiaBodyText = document.createElement('h4');
+  var italiaThank = document.createTextNode('Thank you for voting!');
 
   var italiaFooter = document.createElement('div');
   italiaFooter.setAttribute('class', 'modal-footer');
 
-
   var italiaClose = document.createElement('button');
   italiaClose.setAttribute('class', 'btn');
   italiaClose.setAttribute('class', 'btn-default');
-  italiaClose.createTextNode('Close');
+  var italiaDone = document.createTextNode('Close');
 
+  document.body.appendChild(italiaContainer);
 
-  document.body.appendChild(italia);
-  
+  italiaContainer.appendChild(italiaRow);
+  italiaContainer.appendChild(italiaCol);
+  italiaContainer.appendChild(italia);
+  italiaContainer.appendChild(firstBox);
+  italiaContainer.appendChild(firstVote);
+  italiaContainer.appendChild(italiaImage);
+  italiaContainer.appendChild(italiaCaption); 
+  italiaContainer.appendChild(italiaOuter);
+  italiaContainer.appendChild(italiaBones);
+  italiaContainer.appendChild(italiaContent);
+  italiaContainer.appendChild(italiaHeader);
+  italiaContainer.appendChild(italiaClick);
+  italiaContainer.appendChild(italiaHide);
+  italiaContainer.appendChild(italiaEx);
+  italiaContainer.appendChild(italiaTitle);
+  italiaContainer.appendChild(italiaHead);
+  italiaContainer.appendChild(italiaBody);
+  italiaContainer.appendChild(italiaBodyText);
+  italiaContainer.appendChild(italiaThank);
+  italiaContainer.appendChild(italiaFooter);
+  italiaContainer.appendChild(italiaClose);
+  italiaContainer.appendChild(italiaDone);
+
+  italiaRow.appendChild(italiaCol);
+  italiaRow.appendChild(italia);
+  italiaRow.appendChild(firstBox);
+  italiaRow.appendChild(firstVote);
+  italiaRow.appendChild(italiaImage);
+  italiaRow.appendChild(italiaCaption);
+  italiaRow.appendChild(italiaOuter);
+  italiaRow.appendChild(italiaBones);
+  italiaRow.appendChild(italiaContent);
+  italiaRow.appendChild(italiaHeader);
+  italiaRow.appendChild(italiaClick)
+  italiaRow.appendChild(italiaHide);
+  italiaRow.appendChild(italiaEx);
+  italiaRow.appendChild(italiaTitle);
+  italiaRow.appendChild(italiaHead);
+  italiaRow.appendChild(italiaBody);
+  italiaRow.appendChild(italiaBodyText);
+  italiaRow.appendChild(italiaThank);
+  italiaRow.appendChild(italiaFooter);
+  italiaRow.appendChild(italiaClose);
+  italiaRow.appendChild(italiaDone);
+
+  italiaCol.appendChild(italia);
+  italiaCol.appendChild(firstBox);
+  italiaCol.appendChild(firstVote);
+  italiaCol.appendChild(italiaImage);
+  italiaCol.appendChild(italiaCaption);
+  italiaCol.appendChild(italiaOuter);
+  italiaCol.appendChild(italiaBones);
+  italiaCol.appendChild(italiaContent);
+  italiaCol.appendChild(italiaHeader);
+  italiaCol.appendChild(italiaClick);
+  italiaCol.appendChild(italiaHide);
+  italiaCol.appendChild(italiaEx);
+  italiaCol.appendChild(italiaTitle);
+  italiaCol.appendChild(italiaHead);
+  italiaCol.appendChild(italiaBody);
+  italiaCol.appendChild(italiaBodyText);
+  italiaCol.appendChild(italiaThank);
+  italiaCol.appendChild(italiaFooter);
+  italiaCol.appendChild(italiaClose);
+  italiaCol.appendChild(italiaDone);
 
   italia.appendChild(firstBox);
   italia.appendChild(firstVote);
@@ -91,65 +152,84 @@ function firstModal () {
   italia.appendChild(italiaHeader);
   italia.appendChild(italiaClick);
   italia.appendChild(italiaHide);
+  italia.appendChild(italiaEx);
   italia.appendChild(italiaTitle);
+  italia.appendChild(italiaHead);
   italia.appendChild(italiaBody);
+  italia.appendChild(italiaBodyText);
+  italia.appendChild(italiaThank);
   italia.appendChild(italiaFooter);
   italia.appendChild(italiaClose);
-
+  italia.appendChild(italiaDone);
 
   firstBox.appendChild(firstVote);
   firstBox.appendChild(italiaImage);
   firstBox.appendChild(italiaCaption);
 
-
   firstVote.appendChild(italiaImage);
-  firstVote.appendChild(italiaCaption);
-
 
   italiaOuter.appendChild(italiaBones);
   italiaOuter.appendChild(italiaContent);
   italiaOuter.appendChild(italiaHeader);
   italiaOuter.appendChild(italiaClick);
   italiaOuter.appendChild(italiaHide);
+  italiaOuter.appendChild(italiaEx);
   italiaOuter.appendChild(italiaTitle);
+  italiaOuter.appendChild(italiaHead)
   italiaOuter.appendChild(italiaBody);
+  italiaOuter.appendChild(italiaBodyText);
+  italiaOuter.appendChild(italiaThank);
   italiaOuter.appendChild(italiaFooter);
   italiaOuter.appendChild(italiaClose);
-
+  italiaOuter.appendChild(italiaDone);
 
   italiaBones.appendChild(italiaContent);
   italiaBones.appendChild(italiaHeader);
   italiaBones.appendChild(italiaClick);
   italiaBones.appendChild(italiaHide);
+  italiaBones.appendChild(italiaEx);
   italiaBones.appendChild(italiaTitle);
+  italiaBones.appendChild(italiaHead);
   italiaBones.appendChild(italiaBody);
+  italiaBones.appendChild(italiaBodyText);
+  italiaBones.appendChild(italiaThank);
   italiaBones.appendChild(italiaFooter);
   italiaBones.appendChild(italiaClose);
-
-
+  italiaBones.appendChild(italiaDone);
 
   italiaContent.appendChild(italiaHeader);
   italiaContent.appendChild(italiaClick);
   italiaContent.appendChild(italiaHide);
+  italiaContent.appendChild(italiaEx);
   italiaContent.appendChild(italiaTitle);
+  italiaContent.appendChild(italiaHead);
   italiaContent.appendChild(italiaBody);
+  italiaContent.appendChild(italiaBodyText);
+  italiaContent.appendChild(italiaThank);
   italiaContent.appendChild(italiaFooter);
   italiaContent.appendChild(italiaClose);
-
-
+  italiaContent.appendChild(italiaDone);
 
   italiaHeader.appendChild(italiaClick);
   italiaHeader.appendChild(italiaHide);
+  italiaHeader.appendChild(italiaEx);
   italiaHeader.appendChild(italiaTitle);
-
+  italiaHeader.appendChild(italiaHead);
 
   italiaClick.appendChild(italiaHide);
-
+  italiaClick.appendChild(italiaEx);
 
   italiaHide.appendChild(italiaEx);
 
+  italiaTitle.appendChild(italiaHead);
+
+  italiaBody.appendChild(italiaBodyText);
+  italiaBody.appendChild(italiaThank);
 
   italiaFooter.appendChild(italiaClose);
+  italiaFooter.appendChild(italiaDone);
+
+  italiaClose.appendChild(italiaDone);
 };
 
 firstModal();
